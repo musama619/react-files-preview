480f80dce52d45e6d2efaa510c52f060e6ef3562
Usama Chouhan <musama6191@gmail.com>
2023-06-14 08:21:17 -0400

    package setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts'
-  }
-})
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/tests/setup.ts",
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.tsx"),
+			name: "ReactFilePreview",
+			fileName: (format) => `react-file-preview.${format}.js`,
+		},
+		rollupOptions: {
+			external: ["react", "react-dom"],
+			output: {
+				globals: {
+					react: "React",
+				},
+			},
+		},
+	},
+});
